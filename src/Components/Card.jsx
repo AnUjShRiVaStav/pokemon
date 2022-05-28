18e04bc2582dad8f2812270d7197a04d3ca0a8b8
iAnujShrivastava <anuj.shrivastava@ipsator.com>
2022-05-28 18:15:33 +0530

    search is added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button'
 
 const Card = ({ pokemon, loading,infoPokemon}) => {
+    const [search, setSearchInput] = useState('');
 
+    const handleInput = ((e) => {
+        setSearchInput(e.target.value)
+      })
 
    console.log(pokemon);
     return (
         <>
+
+<input placeholder = 'Search by title...' type ='text'
+   onChange = {handleInput}  />
         {
             loading ? <h1>Loading...</h1> :
-                pokemon.map((item) => {
+                pokemon.filter((val)=> {
+  if(search=='') {
+    return val
+  }else if (val.name.toLowerCase().includes(search.toLowerCase())){
+    return val
+  }
+}).map((item) => {
                     return (
                         <>
+                        
                             <div className="card" key={item.id} onClick={()=>infoPokemon(item)}>
                                 <h2>{item.id}</h2>
                                 <img src={item.sprites.front_default} alt="" />
